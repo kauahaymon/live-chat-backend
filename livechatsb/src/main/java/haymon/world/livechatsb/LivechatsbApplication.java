269cd66008339c1e1db8629615e73fd038f0e3c9
Haymon <kauahaymon@hotmail.com>
2025-01-05 14:43:58 -0400

    First Commit

---
+++ livechatsb/src/main/java/haymon/world/livechatsb/LivechatsbApplication.java
@@ -0,0 +1,13 @@
+package haymon.world.livechatsb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LivechatsbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LivechatsbApplication.class, args);
+	}
+
+}
---
+++ livechatsb/src/test/java/haymon/world/livechatsb/LivechatsbApplicationTests.java
@@ -0,0 +1,13 @@
+package haymon.world.livechatsb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LivechatsbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
